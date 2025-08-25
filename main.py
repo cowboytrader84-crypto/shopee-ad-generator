a8d0b68b0f29946a2e1a4f1613a9ebd03d02a96a
cowboytrader84-crypto <cowboytrader84@gmail.com>
2025-08-25 14:24:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from flask import Flask, send_from_directory
 from flask_cors import CORS
-from src.models.user import db
-from src.routes.user import user_bp
 from src.routes.shopee import shopee_bp
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
@@ -15,22 +13,14 @@ app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
 # Habilitar CORS
 CORS(app)
 
-app.register_blueprint(user_bp, url_prefix='/api')
 app.register_blueprint(shopee_bp, url_prefix='/api/shopee')
-
-# uncomment if you need to use database
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-with app.app_context():
-    db.create_all()
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
     static_folder_path = app.static_folder
     if static_folder_path is None:
-            return "Static folder not configured", 404
+        return "Static folder not configured", 404
 
     if path != "" and os.path.exists(os.path.join(static_folder_path, path)):
         return send_from_directory(static_folder_path, path)
@@ -41,6 +31,6 @@ def serve(path):
         else:
             return "index.html not found", 404
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Usa a porta do Render ou 5000 como padrão
+    app.run(host='0.0.0.0', port=port, debug=False)  # debug=False para produção
